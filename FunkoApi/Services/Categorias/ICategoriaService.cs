14a99e3a007efccad582425ee92e55dedf3fda65
Ángel Sánchez Gasanz <129800127+SrAngelDev@users.noreply.github.com>
2026-01-14 13:38:15 +0100

    Modificacion de la interfaz del servicio para usar AppError para ROP

--- FunkoApi/Services/Categorias/ICategoriaService.cs
+++ FunkoApi/Services/Categorias/ICategoriaService.cs
@@ -1,5 +1,6 @@
 ﻿using CSharpFunctionalExtensions;
 using FunkoApi.Dtos;
+using FunkoApi.Errors;
 using FunkoApi.Models;
 
 namespace FunkoApi.Services.Categorias;
@@ -7,9 +8,9 @@ namespace FunkoApi.Services.Categorias;
 public interface ICategoriaService
 {
     Task<IEnumerable<CategoriaRespondeDto>> GetAllAsync();
-    Task<Result<CategoriaRespondeDto, string>> GetByIdAsync(Guid id);
-    Task<Result<CategoriaRespondeDto, string>> GetByNombreAsync(string nombre);
-    Task<Result<CategoriaRespondeDto, string>> CreateAsync(CategoriaRequestDto categoria);
-    Task<Result<CategoriaRespondeDto, string>> UpdateAsync(Guid id, CategoriaRequestDto categoria);
-    Task<Result<CategoriaRespondeDto, string>> DeleteAsync(Guid id);
+    Task<Result<CategoriaRespondeDto, AppError>> GetByIdAsync(Guid id);
+    Task<Result<CategoriaRespondeDto, AppError>> GetByNombreAsync(string nombre);
+    Task<Result<CategoriaRespondeDto, AppError>> CreateAsync(CategoriaRequestDto dto);
+    Task<Result<CategoriaRespondeDto, AppError>> UpdateAsync(Guid id, CategoriaRequestDto dto);
+    Task<Result<CategoriaRespondeDto, AppError>> DeleteAsync(Guid id);
 }
