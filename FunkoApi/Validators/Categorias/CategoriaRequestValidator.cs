f40325d0f24e324677e06e813b71daa570de9dab
Ángel Sánchez Gasanz <129800127+SrAngelDev@users.noreply.github.com>
2026-01-14 13:35:48 +0100

    validador de categorias y funkos hecho

---
+++ FunkoApi/Validators/Categorias/CategoriaRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FunkoApi.Dtos;
+
+namespace FunkoApi.Validators.Categorias;
+
+public class CategoriaRequestValidator : AbstractValidator<CategoriaRequestDto>
+{
+    public CategoriaRequestValidator()
+    {
+        RuleFor(x => x.Nombre)
+            .NotEmpty().WithMessage("El nombre de la categoría es obligatorio")
+            .MinimumLength(3).WithMessage("El nombre debe tener al menos 3 caracteres")
+            .MaximumLength(50).WithMessage("El nombre no puede superar los 50 caracteres");
+    }
+}
---
+++ FunkoApi/Validators/Funkos/FunkoRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FunkoApi.Dtos;
+
+namespace FunkoApi.Validators.Funkos;
+
+public class FunkoRequestValidator : AbstractValidator<FunkoRequestDto>
+{
+    public FunkoRequestValidator()
+    {
+        RuleFor(x=> x.Nombre)
+            .NotEmpty().WithMessage("El nombre del Funko es obligatorio.")
+            .MaximumLength(100).WithMessage("El nombre no puede exceder 100 caracteres.");
+        
+        RuleFor(x => x.Precio)
+            .GreaterThan(0).WithMessage("El precio debe ser un valor positivo");
+
+        RuleFor(x => x.CategoriaNombre)
+            .NotEmpty().WithMessage("La categoría asociada es obligatoria");
+    }
+}
